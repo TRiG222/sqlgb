efe7459a21ae8c1d0f003a885b9acdb5ed0edfe5
TRiG <gerysh07@gmail.com>
2020-07-14 15:41:23 +0300

    hw9-10

---
+++ hw9.sql
@@ -0,0 +1,44 @@
+-- Создайте таблицу logs типа Archive. Пусть при каждом создании записи в таблицах users, catalogs и products в таблицу logs помещается время и дата создания записи, название 
+
+DROP TABLE IF EXISTS logs;
+CREATE TABLE logs (
+	created_at DATETIME NOT NULL,
+	table_name VARCHAR(45) NOT NULL,
+	str_id BIGINT(20) NOT NULL,
+	name_value VARCHAR(45) NOT NULL
+) ENGINE = ARCHIVE;
+
+
+-- TRIGGER ON users  
+DROP TRIGGER IF EXISTS log_users;
+delimiter //
+CREATE TRIGGER log_users AFTER INSERT ON users
+FOR EACH ROW
+BEGIN
+	INSERT INTO logs (created_at, table_name, str_id, name_value)
+	VALUES (NOW(), 'users', NEW.id, NEW.name);
+END //
+delimiter ;
+
+
+-- TRIGGER ON catalogs 
+DROP TRIGGER IF EXISTS log_catalogs;
+delimiter //
+CREATE TRIGGER log_catalogs AFTER INSERT ON catalogs
+FOR EACH ROW
+BEGIN
+	INSERT INTO logs (created_at, table_name, str_id, name_value)
+	VALUES (NOW(), 'catalogs', NEW.id, NEW.name);
+END //
+delimiter ;
+
+
+-- TRIGGER ON products 
+delimiter //
+CREATE TRIGGER log_products AFTER INSERT ON products
+FOR EACH ROW
+BEGIN
+	INSERT INTO logs (created_at, table_name, str_id, name_value)
+	VALUES (NOW(), 'products', NEW.id, NEW.name);
+END //
+delimiter ;
